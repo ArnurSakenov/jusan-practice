b5f2e20af5dc29b97b3e7f35d6faecdc1773e1c9
ArnurSakenov <arnur.s@mail.ru>
2022-11-22 21:53:40 +0600

    Initial Commit

---
+++ jusan-practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jusan-practice
+//
+//  Created by Arnur Sakenov on 22.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jusan-practice/jusan_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  jusan_practiceApp.swift
+//  jusan-practice
+//
+//  Created by Arnur Sakenov on 22.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct jusan_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
